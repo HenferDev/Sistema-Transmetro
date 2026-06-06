884632a7e557f89d63eddbb46bf988dcba19638d
Hensser Díaz <henferosa@gmail.com>
2026-06-06 16:06:33 -0600

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -2149,6 +2149,10 @@ app.get("/reporte-estaciones", (req, res) => {
     });
 });
 
+app.get("/prueba", (req, res) => {
+    res.send("FUNCIONA");
+});
+
 
 // =====================================================
 // PUERTO EXPRESS
